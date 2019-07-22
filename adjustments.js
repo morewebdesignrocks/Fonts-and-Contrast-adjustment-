7f035e27d0833f48294997b43332652ba9cabe30
Andres Robayo <morewebdesignrocks@gmail.com>
2019-07-22 08:49:37 -0400

    Create event listeners

--- adjustments.js
+++ adjustments.js
@@ -1,12 +1,18 @@
 (function () {
-  //Set variables for action buttons    
-  var rFont = document.getElementById('regularFont');
+	//Set variables for action buttons    
+	var rFont = document.getElementById('regularFont');
 	var mFont = document.getElementById('mediumFont');
 	var lFont = document.getElementById('largeFont');
 	var rContrast = document.getElementById('regularContrast');
 	var mContrast = document.getElementById('mediumContrast');
 	var hContrast = document.getElementById('highContrast');
   
-  
+	//Create event listeners
+	rFont.addEventListener('click', regularFont);
+	mFont.addEventListener('click', mediumFont);
+	lFont.addEventListener('click', largeFont);
+	rContrast.addEventListener('click', regContrast);
+	mContrast.addEventListener('click', moreContrast);
+	hContrast.addEventListener('click', mostContrast);
   
 })();
