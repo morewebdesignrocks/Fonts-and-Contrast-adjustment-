59c2138b57e0bd467c8a4e6ebc487b692d78bc16
Andres Robayo <morewebdesignrocks@gmail.com>
2019-07-22 08:47:22 -0400

    Update adjustments.js

--- adjustments.js
+++ adjustments.js
@@ -1,3 +1,12 @@
 (function () {
-
+  //Set variables for action buttons    
+  var rFont = document.getElementById('regularFont');
+	var mFont = document.getElementById('mediumFont');
+	var lFont = document.getElementById('largeFont');
+	var rContrast = document.getElementById('regularContrast');
+	var mContrast = document.getElementById('mediumContrast');
+	var hContrast = document.getElementById('highContrast');
+  
+  
+  
 })();
